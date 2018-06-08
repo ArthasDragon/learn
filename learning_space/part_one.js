1dfc7ce5b7a1317581a7ac55e5e1113c13c62ceb
an_ying <ywl123698745>
2018-06-08 17:48:15 +0800

    js设计模式

--- learning_space/part_one.js
+++ learning_space/part_one.js
@@ -1,5 +1,5 @@
-//this   apply   call
-
+//第二章  this   apply   call
+//2.1 this    start
 //1.对象的方法调用
 var obj = {
   a: 1,
@@ -36,3 +36,28 @@ console.log(new MyPerson1().name)
 console.log(new MyPerson2().name)
 
 //4.  Function.prototype.call 或 Function.prototype.apply 调用
+var myObj = {
+  name: 'myobj',
+  getName: function() {
+    console.log(this.name)
+  }
+}
+var haha = { name: 'hehe' }
+myObj.getName.call(haha)
+
+//this    end
+
+//2.2  call  apply   bind
+Function.prototype.bind = function() {
+  //bind   js实现
+  var self = this
+  var context = [].shift.call(arguments)
+  var args = [].slice.call(arguments)
+  return function() {
+    return self.apply(context, [].concat.call(args, [].slice.call[arguments]))
+  }
+}
+
+//第三章   闭包和高阶函数
+//3.1  闭包
+//变量的搜索由内而外
