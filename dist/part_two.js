0001a2c7de43019c4060419959f86706da757781
an_ying <ywl123698745>
2018-06-12 20:08:33 +0800

    单例模式

--- dist/part_two.js
+++ dist/part_two.js
@@ -1,3 +1,20 @@
+var _createClass = (function() {
+	function defineProperties(target, props) {
+		for (var i = 0; i < props.length; i++) {
+			var descriptor = props[i];
+			descriptor.enumerable = descriptor.enumerable || false;
+			descriptor.configurable = true;
+			if ("value" in descriptor) descriptor.writable = true;
+			Object.defineProperty(target, descriptor.key, descriptor);
+		}
+	}
+	return function(Constructor, protoProps, staticProps) {
+		if (protoProps) defineProperties(Constructor.prototype, protoProps);
+		if (staticProps) defineProperties(Constructor, staticProps);
+		return Constructor;
+	};
+})();
+
 function _classCallCheck(instance, Constructor) {
 	if (!(instance instanceof Constructor)) {
 		throw new TypeError("Cannot call a class as a function");
@@ -5,33 +22,50 @@ function _classCallCheck(instance, Constructor) {
 }
 
 //单例模式
-var __instance = (function() {
-	var instance = null;
-	return function(new_value) {
-		if (new_value) {
-			instance = new_value;
+// let __instance = (function() {
+// 	let instance = null;
+// 	return function(new_value) {
+// 		if (new_value) {
+// 			instance = new_value;
+// 		}
+// 		return instance;
+// 	};
+// })();
+// class Singleton {
+// 	constructor(name) {
+// 		this.name = name;
+// 		if (__instance()) {
+// 			return __instance();
+// 		}
+// 		return __instance(this);
+// 	}
+// }
+// let a = new Singleton("111");
+// let b = new Singleton("222");
+// console.log(a === b);
+
+var Singleton = (function() {
+	function Singleton(props) {
+		_classCallCheck(this, Singleton);
+
+		this.name = props;
+	}
+
+	_createClass(Singleton, null, [
+		{
+			key: "getInstance",
+			value: function getInstance(props) {
+				if (!Singleton.instance) {
+					Singleton.instance = new Singleton(props);
+				}
+				return Singleton.instance;
+			}
 		}
-		return instance;
-	};
+	]);
+
+	return Singleton;
 })();
 
-var Singleton = function Singleton(name) {
-	_classCallCheck(this, Singleton);
-
-	this.name = name;
-	if (__instance()) {
-		return __instance();
-	}
-	return __instance(this);
-};
-//   single
-//   getSingle() {
-//     if (!this.single) {
-//       this.single = new Singleton()
-//     }
-//     return this.single
-//   }
-
-var a = new Singleton("111");
-var b = new Singleton("222");
+var a = Singleton.getInstance("111");
+var b = Singleton.getInstance("222");
 console.log(a === b);
--- learning_space/part_two.js
+++ learning_space/part_two.js
@@ -1,22 +1,37 @@
 //单例模式
-let __instance = (function() {
-	let instance = null;
-	return function(new_value) {
-		if (new_value) {
-			instance = new_value;
-		}
-		return instance;
-	};
-})();
+// let __instance = (function() {
+// 	let instance = null;
+// 	return function(new_value) {
+// 		if (new_value) {
+// 			instance = new_value;
+// 		}
+// 		return instance;
+// 	};
+// })();
+// class Singleton {
+// 	constructor(name) {
+// 		this.name = name;
+// 		if (__instance()) {
+// 			return __instance();
+// 		}
+// 		return __instance(this);
+// 	}
+// }
+// let a = new Singleton("111");
+// let b = new Singleton("222");
+// console.log(a === b);
+
 class Singleton {
-	constructor(name) {
-		this.name = name;
-		if (__instance()) {
-			return __instance();
+	constructor(props) {
+		this.name = props;
+	}
+	static getInstance(props) {
+		if (!Singleton.instance) {
+			Singleton.instance = new Singleton(props);
 		}
-		return __instance(this);
+		return Singleton.instance;
 	}
 }
-let a = new Singleton("111");
-let b = new Singleton("222");
+let a = Singleton.getInstance("111");
+let b = Singleton.getInstance("222");
 console.log(a === b);
