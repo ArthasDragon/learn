889ede6ea4647e82c7188a5b854cba6113c3d8df
an_ying <ywl123698745>
2018-06-22 17:07:38 +0800

    js设计模式

--- learning_space/part_two.js
+++ learning_space/part_two.js
@@ -1,4 +1,6 @@
-//单例模式
+//设计模式
+
+//第四章   单例模式
 // let __instance = (function() {
 // 	let instance = null;
 // 	return function(new_value) {
@@ -35,3 +37,42 @@ class Singleton {
 let a = Singleton.getInstance("111");
 let b = Singleton.getInstance("222");
 console.log(a === b);
+
+//不用于创建对象的单例
+// const getSingle = function(fn) {
+//   let result = null
+//   return function() {
+//     return result || (result = fn.apply(this, arguments))
+//   }
+// }
+// let bindEvent = getSingle(function() {
+//   document.getElementById('div1').onclick = function() {
+//     alert('click')
+//   }
+//   return true
+// })
+// let render = function() {
+//   console.log('开始渲染列表')
+//   bindEvent()
+// }
+// render()
+// render()
+// render()
+
+// 第五章   策略模式
+let strategies = {
+	S: function(salary) {
+		return salary * 4;
+	},
+	A: function(salary) {
+		return salary * 3;
+	},
+	B: function(salary) {
+		return salary * 2;
+	}
+};
+let calculateBonus = function(level, salary) {
+	return strategies[level](salary);
+};
+console.log(calculateBonus("S", 20000)); // 输出：80000
+console.log(calculateBonus("A", 10000)); // 输出：30000
